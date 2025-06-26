7644812dbb883afba3cd1ba12aa0e28977296710
jccastillog <91742679+jccastillog@users.noreply.github.com>
2025-06-26 11:31:28 -0500

    Actualizacion 26062025 1131

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,14 +4,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" />
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" />
 
-@if($currentClient->is_custom_domain)
-    <!-- Usar recursos del propio dominio -->
-    <link rel="stylesheet" href="https://{{ $currentClient->domain }}/css/style.css">
-@else
-    <!-- Usar recursos centralizados -->
-    <link rel="stylesheet" href="/{{ request()->route('domain') }}/css/style.css">
-
-@endif
+<link rel="stylesheet" href="{{ $currentClient->url }}/css/style.css">
 
 @if($client->favicon)
 <link rel="icon" href="{{ asset($client->favicon->media->full_url) }}" />
