86631ee46d25a817c4b564bebe8e75c9cc44bcba
jccastillog <91742679+jccastillog@users.noreply.github.com>
2025-07-03 11:27:53 -0500

    Actualizacion 03072025 1128

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,17 @@
 <head>
     @include('layouts.head')
 </head>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2DD4RTEQJX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2DD4RTEQJX');
+</script>
+
 <body>
     @include('layouts.header')
     
